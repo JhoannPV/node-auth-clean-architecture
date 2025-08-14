631c3c2c1c48c1a6bd4474928c08335f3969f06a
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2025-08-13 20:32:02 -0500

    Update dependencies and add CORS support in server configuration

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import cors from 'cors';
 
 interface Options {
     port?: number;
@@ -9,6 +10,11 @@ export class Server {
     public readonly app = express(); // readonly: es para definir una propiedad que solo se puede asignar una vez
     private readonly port: number;
     private readonly routes: Router;
+    private readonly corsOptions = cors({
+        origin: '*', // Permitir todas las solicitudes CORS
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+        allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+    });
 
     constructor(options: Options) {
         // el = 3100 es para que si no se pasa el puerto, por defecto sea 3100
@@ -22,6 +28,7 @@ export class Server {
         // Middlewares: funciones que se ejecutan antes que otras funciones
         this.app.use(express.json()); // Para que el servidor entienda JSON
         this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded, Para que el servidor entienda formularios
+        this.app.use(this.corsOptions); // Habilitar CORS
 
         // Usar las rutas definidas
         this.app.use(this.routes);
